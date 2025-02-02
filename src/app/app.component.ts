65807999ff4e6dcd9264ddd08382832cde2826d7
Ranjithkumar Rajendran <rrkarulraja@gmail.com>
2025-02-01 23:17:51 -0500

    Angular version 19 - Initial Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'core-common-lib';
